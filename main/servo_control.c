dabf2f39c7784660d9b599d712ec59320b00c28b
Markus <makke00694@gmail.com>
2022-06-20 23:02:18 +0300

    added servo functions

--- main/servo_control.c
+++ main/servo_control.c
@@ -1,17 +1,50 @@
 #include <stdio.h>
 #include <driver/ledc.h>
+#include <unistd.h>
+#include <stdio.h>
+#define GPIO_PIN 4
 
 void pmw_config(){
     ledc_timer_config_t ledc_timer = {
         .duty_resolution = LEDC_TIMER_8_BIT,
-        .speed_mode = LEDC_SPEED_MODE_MAX,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
         .freq_hz = 50,
         .timer_num = LEDC_TIMER_0,
-        .clk_cfg = LEDC_AUTO_CLK,
+        .clk_cfg = LEDC_AUTO_CLK
     };
+    ledc_timer_config(&ledc_timer);
+    
+    ledc_channel_config_t ledc_channel = {
+        .channel = LEDC_CHANNEL_0,
+        .duty = 0,
+        .gpio_num = GPIO_PIN,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_sel = LEDC_TIMER_0,
+    };
+    ledc_channel_config(&ledc_channel);
+    
+    sleep(1);
+
+}
+
+void open_door(){
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 26);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+}
+
+void close_door(){
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 10);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
 }
 
 void app_main(void)
 {
-
+    pmw_config();
+    while(1){
+        open_door();
+        sleep(2);
+        close_door();
+        sleep(2);
+    }
 }
+
